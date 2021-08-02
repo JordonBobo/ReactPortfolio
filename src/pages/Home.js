ed3fc4b416d8d7c1b02c8945c866032ab7ce7798
Jordon Bobo <jordon.bobo@outlook.com>
2021-08-02 12:32:41 -0600

    updated resume

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,7 +14,7 @@ function Home() {
             <div className="row row-cols-sm-6">
 
               <div className="container">
-                <a href="https://jordonbobo.github.io/GeneralPortfolio/Assets/JordonBobo-Resume-May-2021.pdf" target="_blank" className="homeText">
+                <a href="https://jordonbobo.github.io/GeneralPortfolio/Assets/Jordon-Bobo-Resume-Aug-2021.pdf" target="_blank" className="homeText">
                   <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" className="bi bi-person-square" viewBox="0 0 16 16">
                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                     <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z" />
